d4ea98342173e87668e259902eb3c32b5e46091a
Javi Martín <javim@elretirao.net>
2019-10-06 13:59:23 +0200

    Convert markdown to HTML on demand
    We were converting markdown to HTML every time we saved a record, which
    has the same problems as sanitizing HTML before saving it to the
    database, particularly because the body of a legislation draft is stored
    in a translations table.
    
    Performance-wise this isn't a problem: converting a text with more than
    200_000 characters takes about a milisecond on my machine.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190607160900) do
+ActiveRecord::Schema.define(version: 20191006114603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -755,8 +755,6 @@ ActiveRecord::Schema.define(version: 20190607160900) do
     t.string   "title"
     t.text     "changelog"
     t.text     "body"
-    t.text     "body_html"
-    t.text     "toc_html"
     t.datetime "hidden_at"
     t.index ["hidden_at"], name: "index_legislation_draft_version_translations_on_hidden_at", using: :btree
     t.index ["legislation_draft_version_id"], name: "index_900e5ba94457606e69e89193db426e8ddff809bc", using: :btree
@@ -773,8 +771,6 @@ ActiveRecord::Schema.define(version: 20190607160900) do
     t.datetime "hidden_at"
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
-    t.text     "body_html"
-    t.text     "toc_html"
     t.index ["hidden_at"], name: "index_legislation_draft_versions_on_hidden_at", using: :btree
     t.index ["legislation_process_id"], name: "index_legislation_draft_versions_on_legislation_process_id", using: :btree
     t.index ["status"], name: "index_legislation_draft_versions_on_status", using: :btree
